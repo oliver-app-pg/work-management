3b50af58cd78586c2ae5685ec0aaec21f21b7834
oliver-app-pg <oliver.app.pg@gmail.com>
2018-08-30 17:56:37 +0900

    design test

---
+++ DB.php
@@ -0,0 +1,7 @@
+<?php
+$db = array(
+    'host' => mysql1.php.xdomain.ne.jp,
+    'user' => 'oliverapppg_root',
+    'pass' => 'frkw0318X'
+);
+?>
--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 <meta name="description" content="">
 <meta name="author" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="">
+<link rel="stylesheet" href="login.css">
 <!--[if lt IE 9]>
 <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -17,9 +17,39 @@
 <body>
 
 <?php
-echo "login";
+require_once 'DB.php';
+
+$ID = $_POST["ID"];
+$password = $_POST["password"];
+
+$mysqli = new PDO('mysql:host=mysql1.php.xdomain.ne.jp;dbname=oliverapppg_workmanagement;charset=utf8',$db['user'],$db['pass']);
+$sql = "SELECT * FROM oliverapppg_workmanagement.member WHERE ID = :ID AND password = :password";
+$stmt = $mysqli->prepare($sql);
+$stmt->bindValue(':ID', $ID);
+$stmt->bindValue(':password', $password);
+$res = $stmt->execute();
+if($stmt->fetch() == false)
+{
+	echo "ログインに失敗しました";
+	exit();
+}
+else
+{
+}
+
 ?>
 
+<nav>ID: <?php echo $ID ?> </nav>
+<aside>
+a<br>
+b<br>
+c<br>
+d<br>
+</aside>
+<article>
+main
+</article>
+<footer></footer>
 <!-- SCRIPTS -->
 </body>
 </html>
